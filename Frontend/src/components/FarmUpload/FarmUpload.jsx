542d4766b97c586fe82cd07a0d514cb101cec095
robin nganga smith <saintofcookies.2021@gmail.com>
2024-02-19 16:27:19 +0300

    this commit is to show that I added the product-list container the displays the product details,price,image and has a button. duplicated two more times

--- Frontend/src/components/FarmUpload/FarmUpload.jsx
+++ Frontend/src/components/FarmUpload/FarmUpload.jsx
@@ -16,6 +16,60 @@ const FarmUpload = () => {
       <div id='total-price'>
         <h3>Total Payable: 6,900</h3>
       </div>
+      <div id='product-list'>
+  <div id='image-block'>
+  <img id='product-img' src='https://th.bing.com/th/id/OIP.jNrm4XKi51vHf1D-yAjB0QHaHa?w=1440&h=1440&rs=1&pid=ImgDetMain' alt='product img'></img>
+  <div id='product-definition'>
+    <p>Breed.Holstein(Dairy)</p>
+    <p>Productivity:High milk...</p>
+    <p>Health:Vaccinated,good...</p>
+  </div>
+  </div>
+  <div id='remove-from-list'>
+    <p>
+      Cost: $2,300
+    </p>
+    <button>Remove from list</button>
+  </div>
+</div>
+<div id='product-list'>
+  <div id='image-block-2'>
+  <img id='product-img' src='https://th.bing.com/th/id/OIP.jNrm4XKi51vHf1D-yAjB0QHaHa?w=1440&h=1440&rs=1&pid=ImgDetMain' alt='product img'></img>
+  <div id='product-definition'>
+    <p>Breed.Holstein(Dairy)</p>
+    <p>Productivity:High milk...</p>
+    <p>Health:Vaccinated,good...</p>
+  </div>
+  <div id='remove-from-list-2'>
+    <p>
+      Cost: $2,300
+    </p>
+    <button>Remove from list</button>
+  </div>
+  </div>
+  
+</div>
+<div id='product-list'>
+<div id='image-block-3'>
+  <img id='product-img' src='https://th.bing.com/th/id/OIP.jNrm4XKi51vHf1D-yAjB0QHaHa?w=1440&h=1440&rs=1&pid=ImgDetMain' alt='product img'></img>
+  </div>
+  <div id='product-definition-3'>
+    <p>Breed.Holstein(Dairy)</p>
+    <p>Productivity:High milk...</p>
+    <p>Health:Vaccinated,good...</p>
+  </div>
+  <div id='remove-from-list-3'>
+    <p>
+      Cost: $2,300
+    </p>
+    <button>Remove from list</button>
+  </div>
+</div>
+<button id='place-order'>Place your order</button>
+    </div>
+   
+    </div>
+    </div>
   )
 }
 
