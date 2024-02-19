a2184d9fafa190b64d87bef968485560a0f3aee2
robin nganga smith <saintofcookies.2021@gmail.com>
2024-02-19 16:56:50 +0300

    this commit is to show that I hvae created the main, maincard and header containers

--- Frontend/src/components/FarmUpload/FarmUpload.jsx
+++ Frontend/src/components/FarmUpload/FarmUpload.jsx
@@ -4,7 +4,14 @@ const FarmUpload = () => {
   return (
     <div>
       <Header />
-      
+      <div>
+    
+    <div id = 'main'>
+
+<div className= 'maincard'>
+  <div id = 'header'>
+<h1>Add an animal to your sales pen</h1>
+ </div>
     </div>
   )
 }
