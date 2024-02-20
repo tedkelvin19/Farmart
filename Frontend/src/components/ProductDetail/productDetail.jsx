47686a86a53f212e619517bbae9f32a8eec0f277
robin nganga smith <saintofcookies.2021@gmail.com>
2024-02-20 09:32:14 +0300

    this commit is to show that I have added three containers. "body","header","main"

--- Frontend/src/components/ProductDetail/productDetail.jsx
+++ Frontend/src/components/ProductDetail/productDetail.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const productDetail = () => {
   return (
     <div>
-      
+       <div id='body'> 
+      <div id='header'>
+        <h1>Animal Details</h1>
+        </div>
+      <div id='main'></div>
     </div>
   )
 }
